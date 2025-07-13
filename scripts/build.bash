538de01bcd5c6d737567880a4ca1f6b85c8e09d5
corvus <wagneraron@outlook.de>
2025-07-13 21:18:06 +0200

    Enhance build script by adding ROS2 environment sourcing and fixing colcon build command

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source ROS2 environment
+source /opt/ros/jazzy/setup.bash
+
 cd ../../
-colcon build --cmake-force-configure --packages-select guiniverse && \
+colcon build --cmake-force-configure --packages-select guiniverse --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && \
 source install/local_setup.bash 
